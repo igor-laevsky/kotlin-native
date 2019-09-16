a2c8ad10f639ac434d8e6a7793f4821c99afbd69
Sergey Bogolepov <aceisnotmycard@gmail.com>
2019-09-16 18:10:27 +0700

    Add tests for objc_msgSend* family.

---
+++ backend.native/tests/interop/objc/msg_send/messaging.h
@@ -0,0 +1,47 @@
+#import <objc/NSObject.h>
+
+@interface PrimitiveTestSubject : NSObject
+
++ (int)intFn;
++ (float)floatFn;
++ (double)doubleFn;
+
+@end;
+
+typedef struct {
+    float f;
+} SingleFloat;
+
+typedef struct __attribute__((packed)) {
+    char f1;
+    short f2;
+    char f3;
+    char f4;
+} SimplePacked;
+
+typedef struct {
+    float f1;
+    float f2;
+    float f3;
+    float f4;
+} HomogeneousSmall;
+
+typedef struct {
+    float f1;
+    float f2;
+    float f3;
+    float f4;
+    float f5;
+    float f6;
+    float f7;
+    float f8;
+} HomogeneousBig;
+
+@interface AggregateTestSubject : NSObject
+
++ (SingleFloat)singleFloatFn;
++ (SimplePacked)simplePackedFn;
++ (HomogeneousSmall)homogeneousSmallFn;
++ (HomogeneousBig)homogeneousBigFn;
+
+@end;
